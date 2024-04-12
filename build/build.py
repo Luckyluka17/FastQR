9b56661e32a5479e52fca15f8800776cd39227f1
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2024-04-12 17:32:08 +0200

    Update build.py

--- build/build.py
+++ build/build.py
@@ -4,5 +4,5 @@ path = os.getcwd()
 
 print("Compilation du code... [1/2]")
 os.system(f'pyinstaller --noconfirm --onefile --console --icon "{path}/assets/images/logo.ico"  "{path}/fastqr.py"')
-print("Compilation du code... [2/2]")
-os.system(f'pyinstaller --noconfirm --onefile --windowed --icon "{path}/assets/images/logo.ico"  "{path}/gui.py"')
+# print("Compilation du code... [2/2]")
+# os.system(f'pyinstaller --noconfirm --onefile --windowed --icon "{path}/assets/images/logo.ico"  "{path}/gui.py"')
