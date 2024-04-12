b9e8808e21479194344e23765ac2d200f41362e4
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2024-04-12 17:09:02 +0200

    Create build.py

---
+++ build/build.py
@@ -0,0 +1,8 @@
+import os
+
+path = os.getcwd()
+
+print("Compilation du code... [1/2]")
+os.system(f'pyinstaller --noconfirm --onefile --console --icon "{path}/assets/images/logo.ico"  "{path}/fastqr.py"')
+print("Compilation du code... [2/2]")
+os.system(f'pyinstaller --noconfirm --onefile --windowed --icon "{path}/assets/images/logo.ico"  "{path}/gui.py"')
